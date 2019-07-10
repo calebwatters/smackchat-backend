a96a160677c23a5eb1230f23e3f3140ab54152d8
jkshareef <jamalkshareef@gmail.com>
2019-07-10 09:26:44 -0700

    adjusted users controller #update

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
         resources :users
         post '/login', to: 'auth#create'
         get '/profile', to: 'users#profile'
-        put '/profile', to: 'users#edit'
     end
   end
 
