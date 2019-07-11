069e18ea48a89ba78f687b846497a6c769d330c6
calebwatters <caleb@h2os.org>
2019-07-11 03:01:33 -0700

    cors issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,10 +4,12 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-    config.action_cable.url = "ws://smack-chat-heroku.herokuapp.com/cable"
 
-  config.action_cable.allowed_request_origins = [/http:\/\/*/, 
-/https:\/\/*/]
+  
+#     config.action_cable.url = "ws://smack-chat-heroku.herokuapp.com/cable"
+
+#   config.action_cable.allowed_request_origins = [/http:\/\/*/, 
+# /https:\/\/*/]
 
   config.cache_classes = false
 
