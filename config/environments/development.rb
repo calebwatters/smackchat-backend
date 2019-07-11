619178d09c6d864d7d996a90b7eab2e1322bb467
calebwatters <caleb@h2os.org>
2019-07-11 03:03:21 -0700

    resolved cors issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,11 +5,10 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
 
-  
-#     config.action_cable.url = "ws://smack-chat-heroku.herokuapp.com/cable"
 
-#   config.action_cable.allowed_request_origins = [/http:\/\/*/, 
-# /https:\/\/*/]
+    config.action_cable.url = "ws://smack-chat-heroku.herokuapp.com/cable"
+
+
 
   config.cache_classes = false
 
