1142b11502c0febc40af6cb10b754f7b36308b22
calebwatters <caleb@h2os.org>
2019-07-11 03:14:13 -0700

    still fixing config issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
 
 
-    config.action_cable.url = "ws://smack-chat-heroku.herokuapp.com/cable"
+    config.action_cable.url = "wss://smack-chat-heroku.herokuapp.com/cable"
 
 
 
