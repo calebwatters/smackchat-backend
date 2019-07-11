e264dc6bd9aee6fd1ba438d019522a55328e1933
calebwatters <caleb@h2os.org>
2019-07-11 11:18:00 -0700

    added create method to user_channels

--- app/controllers/api/v1/user_channels_controller.rb
+++ app/controllers/api/v1/user_channels_controller.rb
@@ -6,9 +6,22 @@ class Api::V1::UserChannelsController < ApplicationController
 
     end
 
+    def create
+        user_channel = UserChannel.new(user_channel_params)
+        if user.save
+            status: :accepted
+        end
+    end
+
      def show 
         user_channel = UserChannel.find(params[:id]) 
         render json: { user_channel: UserChannelSerializer.new(user_channel) }, status: :accepted
         
+    end
+
+    private
+
+    def user_channel_params
+        params.require(:user_channel).permit(:channel_id, :user_id)
     end
 end
