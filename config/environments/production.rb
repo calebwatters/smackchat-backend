ba2a1c8161a1b1786c8d850fbe5d3a377b808505
calebwatters <caleb@h2os.org>
2019-07-12 11:33:19 -0700

    changed config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   config.action_cable.url = 'wss://smack-chat-heroku.herokuapp.com/cable'
   # config.action_cable.allowed_request_origins = '*'
   
-  config.action_cable.allowed_request_origins = "https://smackchat-messager.herokuapp.com/"
+  config.action_cable.allowed_request_origins = "https://smackchat-messager.herokuapp.com"
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
