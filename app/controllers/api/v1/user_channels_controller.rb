b05110ab28c64f648f2bef81f1f687249a50075f
calebwatters <caleb@h2os.org>
2019-07-12 17:35:39 -0700

    fixed auth

--- app/controllers/api/v1/user_channels_controller.rb
+++ app/controllers/api/v1/user_channels_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UserChannelsController < ApplicationController
-    # skip_before_action :authorized
+    skip_before_action :authorized
     def index 
         user_channels = UserChannel.all 
         render :json => user_channels, status: :accepted
